290a24813d5992ad85b74d0d4bcc3bfa4ac51bd3
papadi166 <papadi166@gmail.com>
2023-01-16 04:51:12 +0100

    .

--- pages/index.vue
+++ pages/index.vue
@@ -66,17 +66,7 @@ const config = useRuntimeConfig();
 const localeSetting = useState<string>("locale.setting");
 
 let response: Ref<any> = ref();
-const ip = await useFetch("https://api.ipify.org?format=json").then((res) => {
-  const data: any = res.data.value;
-  const error = res.error.value;
-  if (error) {
-    console.log(error);
-  } else {
-    return data.ip;
-  }
-});
-
-const user_ip = await ip;
+const user_ip = await useCheckIp();
 response.value = await useGeolocation2(user_ip);
 
 const setLangByGeolocation = () => {
