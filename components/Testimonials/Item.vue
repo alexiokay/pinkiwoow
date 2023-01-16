9d6ad78cc61ec75a4f93870406f1688b47a44bc9
papadi166 <papadi166@gmail.com>
2023-01-16 01:38:16 +0100

    .

--- components/Testimonials/Item.vue
+++ components/Testimonials/Item.vue
@@ -2,7 +2,7 @@
 div(class="w-1/4 h-[25.5rem] flex flex-col items-center smooth-shadow hover:shadow-[0px_0px_35px_1px_rgb(0,0,0,0.15)] justify-center py-4 space-y-5 border-[1px] border-[#E35796]")
     
     img(:src="props.image"  class="rounded-full w-2/6")
-    div(class="flex flex-row w-auto h-auto")
+    div(class="flex flex-row w-auto h-auto") 
         StarFullIcon(class="text-[#FF9529]" v-for="i in Math.floor(props.stars)" :key="i")
        
         StarHalfIcon(v-if="halfstar" class="text-[#FF9529]")
