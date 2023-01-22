9e78bd87fe190453d993bbc98e16848e17724758
papadi166 <papadi166@gmail.com>
2023-01-22 08:05:10 +0100

    language switcher experiments

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -40,7 +40,8 @@ localeSetting.value.toLowerCase();
       as="div"
       class="relative flex items-center"
     >
-      <ListboxLabel class="ssr-only">Theme</ListboxLabel>
+      <ListboxLabel class="">Theme</ListboxLabel>
+      <!-- ssr-only class was before -->
       <ListboxButton
         type="button"
         title="Change Language"
