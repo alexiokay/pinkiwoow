649e9fadabaa732b156a096c8d49d17cfc0f6923
papadi166 <papadi166@gmail.com>
2023-02-04 22:41:18 +0100

    ModalLocation filled with data and functions

--- components/Flag.vue
+++ components/Flag.vue
@@ -1,28 +1,13 @@
 <template>
   <div>
-    <PolishFlag v-if="localeSetting === 'pl'" class="w-1/3 h-3 md:h-16" />
-    <EnglishFlag v-else-if="localeSetting === 'en'" class="w-1/3 h-3 md:h-16" />
-    <DeutschFlag v-else-if="localeSetting === 'de'" class="w-1/3 h-3 md:h-16" />
-    <ChinneseFlag
-      v-else-if="localeSetting === 'zh'"
-      class="w-1/3 h-3 md:h-16"
-    />
-    <IndonesianFlag
-      v-else-if="localeSetting === 'id'"
-      class="w-1/3 h-3 md:h-16"
-    />
-    <JapaneseFlag
-      v-else-if="localeSetting === 'ja'"
-      class="w-1/3 h-3 md:h-16"
-    />
-    <SouthCoreanFlag
-      v-else="localeSetting === 'ko'"
-      class="w-1/3 h-3 md:h-16"
-    />
-    <NetherlandsFlag
-      v-else="localeSetting === 'nl'"
-      class="w-1/3 h-3 md:h-16"
-    />
+    <PolishFlag v-if="iso === 'pl'" class="w-1/3 h-3 md:h-16" />
+    <EnglishFlag v-else-if="iso === 'en'" class="w-1/3 h-3 md:h-16" />
+    <DeutschFlag v-else-if="iso === 'de'" class="w-1/3 h-3 md:h-16" />
+    <ChinneseFlag v-else-if="iso === 'zh'" class="w-1/3 h-3 md:h-16" />
+    <IndonesianFlag v-else-if="iso === 'id'" class="w-1/3 h-3 md:h-16" />
+    <JapaneseFlag v-else-if="iso === 'ja'" class="w-1/3 h-3 md:h-16" />
+    <SouthCoreanFlag v-else="iso === 'ko'" class="w-1/3 h-3 md:h-16" />
+    <NetherlandsFlag v-else="iso === 'nl'" class="w-1/3 h-3 md:h-16" />
   </div>
 </template>
 
@@ -35,9 +20,20 @@ import SouthCoreanFlag from "~icons/circle-flags/ko";
 import IndonesianFlag from "~icons/circle-flags/id";
 import JapaneseFlag from "~icons/circle-flags/ja";
 import NetherlandsFlag from "~icons/circle-flags/nl";
+import { ref } from "vue";
+const props = defineProps({
+  iso: {
+    type: String,
+    required: false,
+  },
+});
 
 const localeSetting = useState<string>("locale.setting");
 localeSetting.value.toLowerCase();
+const iso = ref(localeSetting.value.valueOf().toLowerCase());
+if (props.iso) iso.value = props.iso;
+
+console.log(iso.value);
 </script>
 
 <style lang="sass"></style>
