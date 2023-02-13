d7e8687ab6661f757d054c4ae4efa91f83da8525
papadi166 <papadi166@gmail.com>
2023-02-13 01:33:37 +0100

    fix

--- components/Modal/Loading.vue
+++ components/Modal/Loading.vue
@@ -4,7 +4,7 @@ Transition(name="fade")
     <div v-if="props.isOpen" tabindex="-1" class="fixed top-0 pointer-event-none flex items-center bg-[rgba(0,0,0,0.55)] justify-center left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-screen md:h-full"></div>
 <Transition name="jump">
     <div v-if="props.isOpen" class="fixed top-0 bottom-0 pointer-event-none flex items-center  justify-center left-0 right-0 z-50  m-auto w-[100vw] md:w-[30rem] p-4  md:inset-0 h-min">
-        Loading..
+        p Loading..
     </div>
 </Transition>
     
